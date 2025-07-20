e7a86db99ab8df2d183d83f9481410468c747fb6
Bao Dinh <vobaodinh0904@gmail.com>
2025-07-20 11:18:47 +0700

    1

--- src/pages/ThietBiYTe.js
+++ src/pages/ThietBiYTe.js
@@ -51,16 +51,11 @@ function ThietBiYTe() {
               <p>Máy đo huyết áp, nhiệt kế, máy đo đường huyết</p>
             </div>
             <div className="product-item1"></div>
-            <h3>Thiết bị đo lường</h3>
-            <p>Máy đo huyết áp, nhiệt kế, máy đo đường huyết</p>
-
-            <img src="https://kiemdinhyte.vn/upload/images/2024_17_01_14_25_11-Fb-ult800_02a_c.png" alt="Máy đo huyết áp" />
-
-
+              <h3>Thiết bị đo lường</h3>
+              <p>Máy đo huyết áp, nhiệt kế, máy đo đường huyết</p>
             <div className="product-item">
               <h3>Dụng cụ y tế cơ bản</h3>
               <p>Ống nghe, kính soi tai mũi họng, đèn pin y tế</p>
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeEldc0ptD8t-ZkOzbzGdr9wMOs6sytJikBg&s" alt="Vật dụng y tế gia đình" />
             </div>
             <div className="product-item">
               <h3>Vật tư tiêu hao</h3>
